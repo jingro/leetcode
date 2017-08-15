ad80f46e3bb0dd60f4c218a6e7ad9184f7a62834
jingro <jingxer@163.com>
2017-08-15 22:13:50 +0800

    leetcode updated: 2017-08-15  8:13:50

---
+++ swapNodeInPairs.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        res = t = ListNode(-1)
+        p, q = head, head.next
+        while q:
+            p.next = q.next
+            q.next = p
+            t.next = q
+            t = p
+            p = p.next
+            q = p.next if p else None
+        return res.next
